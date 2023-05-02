be865ca8bd60456d2eb994c059e81c1f638a3659
skmajumder <skmajumder.cse@gmail.com>
2023-05-03 00:26:48 +0600

    add show all & Show less button

--- src/pages/Home/ChefSection/ChefSection.jsx
+++ src/pages/Home/ChefSection/ChefSection.jsx
@@ -4,22 +4,54 @@ import Chefs from "../../../components/Chefs/Chefs";
 
 const ChefSection = () => {
   const [chefs, setChefs] = useState([]);
+  const [numChefsToShow, setNumChefsToShow] = useState(6);
+  const [showMoreButton, setShowMoreButton] = useState(true);
+
   useEffect(() => {
     fetch("http://localhost:3000/chefs")
       .then((response) => response.json())
       .then((data) => setChefs(data));
   }, []);
-  console.log(chefs);
+
+  const handleShowMoreClick = () => {
+    setNumChefsToShow(chefs.length);
+    setShowMoreButton(false);
+  };
+
+  const handleShowLessClick = () => {
+    setNumChefsToShow(6);
+    setShowMoreButton(true);
+  };
 
   return (
     <section className="section-chef">
       <div className="container px-6 py-14">
         <h3 className="text-center font-bold text-3xl mb-6">Our Chefs</h3>
-        <div className="grid grid-cols-3 gap-x-6 gap-y-5">
-          {chefs.map((chef) => (
+        <div className="grid grid-cols-3 gap-x-6 gap-y-5 mb-10">
+          {chefs.slice(0, numChefsToShow).map((chef) => (
             <Chefs key={chef.id} chef={chef} />
           ))}
         </div>
+        {showMoreButton && numChefsToShow < chefs.length && (
+          <div className="flex justify-center items-center">
+            <button
+              onClick={handleShowMoreClick}
+              className="btn btn-primary btn-sm"
+            >
+              Show All
+            </button>
+          </div>
+        )}
+        {!showMoreButton && (
+          <div className="flex justify-center items-center">
+            <button
+              onClick={handleShowLessClick}
+              className="btn btn-primary btn-sm"
+            >
+              Show less
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
