9ee5bec35f9b096732fac03f24bccdc7364c4635
skmajumder <skmajumder.cse@gmail.com>
2023-05-02 17:07:34 +0600

    Header Create

--- src/layouts/Main/MainLayout.jsx
+++ src/layouts/Main/MainLayout.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import Header from "../../components/Header/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
 
 const MainLayout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   );
 };
 
--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import MainLayout from "../layouts/Main/MainLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
@@ -8,8 +9,11 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     errorElement: <ErrorPage />,
     children: [
-      {}
-    ]
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
